2b2b67d86abc12d888f23340b280fdae38e4f97e
Cialysse <marshall.cierra714@gmail.com>
2021-11-07 18:44:08 -0500

    added schema and inital files

---
+++ server/schema/resolvers.js
@@ -0,0 +1,12 @@
+const { AuthenticationError } = require('apollo-server-express')
+const { User } = require('../models')
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: 
+    }, 
+    Mutations: {
+
+    },
+}
